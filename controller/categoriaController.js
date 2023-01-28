36c26d6eff3b89b62205088eb129ad203bc85e82
Nicolas Cisterna <43551684+junder79@users.noreply.github.com>
2023-01-28 19:27:24 -0300

    se integra la lista de vehiculos por categorias

--- controller/categoriaController.js
+++ controller/categoriaController.js
@@ -1,26 +1,35 @@
 import Categoria from "../models/Categoria.js";
+import Vehiculo from "../models/Vehiculo.js";
 
 
-
-const registrarCategoria = async (req,res) => {
+const registrarCategoria = async(req, res) => {
     try {
         const categoria = new Categoria(req.body);
         const categoriaAlmacenado = categoria.save();
-        res.json({ status: 200, message: 'Categoria registrada'});
+        res.json({ status: 200, message: 'Categoria registrada' });
     } catch (error) {
-        res.json({ status: 400, message: 'Error al registrar las categorias'});
+        res.json({ status: 400, message: 'Error al registrar las categorias' });
     }
 }
 
-const getCategorias = async (req,res) => {
+const getCategorias = async(req, res) => {
     try {
         const categorias = await Categoria.find();
         res.json(categorias);
     } catch (error) {
-        res.json({ status: 400, message: 'Error al obtener las categorias'});
+        res.json({ status: 400, message: 'Error al obtener las categorias' });
+    }
+}
+
+const getSpecificCategoria = async(req, res) => {
+    try {
+        const categoriaSpecifica = req.params.categoria;
+        const vehiculosSpecificCategory = await Vehiculo.find().where("categoria").equals(categoriaSpecifica);
+        res.json(vehiculosSpecificCategory);
+    } catch (error) {
+        res.json({ status: 400, message: 'Error al obtener la categoria espcificada', error });
     }
 }
 
 
-export  {registrarCategoria, getCategorias};
-
+export { registrarCategoria, getCategorias, getSpecificCategoria };
--- routes/categoriaRoute.js
+++ routes/categoriaRoute.js
@@ -1,10 +1,10 @@
-import  express  from "express";
-import { registrarCategoria, getCategorias } from "../controller/categoriaController.js";
+import express from "express";
+import { registrarCategoria, getCategorias, getSpecificCategoria } from "../controller/categoriaController.js";
 
 const router = express.Router();
 
 router.post("/nuevaCategoria", registrarCategoria);
-router.get("/",getCategorias);
-
+router.get("/", getCategorias);
+router.get("/:categoria", getSpecificCategoria);
 
 export default router;
