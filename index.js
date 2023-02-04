464eabc3720b64abe4047daf03d6633af5191f37
Nicolas Cisterna <43551684+junder79@users.noreply.github.com>
2023-02-03 23:25:25 -0300

    integracion de cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import conectarDB from './config/db.js';
 import dotenv from 'dotenv';
 import vehiculoRouter from './routes/vehiculoRoutes.js';
 import categoriaRoute from './routes/categoriaRoute.js';
 import noticiaRouter from './routes/noticiaRoute.js';
 
+
 const app = express();
-//app.use(express.json());
+app.use(cors())
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 dotenv.config();
