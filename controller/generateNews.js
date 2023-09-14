db242352c502ae99e9a166b7d54bf79e3e46ea9e
Nicolas Cisterna <43551684+junder79@users.noreply.github.com>
2023-09-13 21:51:14 -0300

    mejora para obtener el primero articulo

--- controller/generateNews.js
+++ controller/generateNews.js
@@ -1,10 +1,38 @@
+import  cheerio from 'cheerio';
+import Noticia from '../models/Noticias.js';
 
-const url2 = 'https://www.rockstargames.com/es/newswire';
+const url = 'https://rockstarintel.com/category/news/';
+
 const generateNews = async (req,res) =>{
     try {
-       
+        const response = await fetch(url);
+
+        const html = await response.text();
+        const $ = cheerio.load(html);
+
+
+        // Obtener el primero de la lista y validar de que no exista en BD
+        const firtArticleTittle = $('.post-title').first().text();
+        
+        const isValid = await Noticia.find({titulo: firtArticleTittle});
+        console.log(isValid);
+        if(isValid.length === 0 ) {
+            const primerArticulo = $('.l-post.grid-post.grid-base-post').first();
+            const spanImagen = primerArticulo.find('.image-link.media-ratio.ratio-16-9 .img');
+            const urlImagen = spanImagen.attr('data-bgsrc');
+            const article = {
+                titulo : firtArticleTittle,
+                link : $('.post-title').first().find('a').attr('href'),
+                imagen: urlImagen,
+                fecha:  $('.post-date').first().text(),
+                description: $('.excerpt p ').first().text(),
+            }
+
+           
+        }       
         res.json({ status: 200, message: 'List newswire' });
     } catch (error) {
+        console.log("error ", error);
         res.json({ status: 400, message: 'Error al listar el newswire'});
     }
 }
